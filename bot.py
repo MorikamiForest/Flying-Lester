c8e52ff9087c9ce071f81bf4c69da212598208e6
chengzong zhao <chengzong.zhao@gmail.com>
2020-12-03 01:22:27 -0500

    1

--- bot.py
+++ bot.py
@@ -1,14 +1,39 @@
+#coding: utf-8
 import os
+import json
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='[')
+bot = commands.Bot(command_prefix='lester>')
 
 @bot.event
 async def on_ready():
-    """
-    docstring
-    """
-    print('>> Lester is Ready to Fly!!!!!')
+    channel = bot.get_channel(673203933698719749)
+    await channel.send('>> Lester is Ready to Fly!!!!!')
+    
+    print('>> Bot online')
 
-bot.run('NzgzODkxODUyMDk1OTE0MDI1.X8hWLQ.bhps3Jak116fCHekKMvc_zuoCnw')
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f"{bot.latency*1000}(ms)")
+
+@bot.command()
+async def flying(ctx):
+    pic = discord.File(os.path.abspath('image/flying.jpg'))
+    await ctx.send(f"I'm Flying!!!", file=pic)
+
+@bot.command()
+async def dancing(ctx):
+    pic = discord.File(os.path.abspath('image/dancing.jpg'))
+    await ctx.send(f"I'm Dancing", file=pic)
+
+@bot.command()
+async def greeting(ctx):
+    pic = discord.File(os.path.abspath('image/greeting.jpg'))
+    await ctx.send(f"I'm Greeting", file=pic)
+
+
+with open('setting.json', 'r', encoding='utf8') as f:
+    setting = json.loads(f.read())
+
+bot.run(setting['TOKEN'])
